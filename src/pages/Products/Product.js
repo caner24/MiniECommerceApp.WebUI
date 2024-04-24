2e7de57bccffba771e864beabb121a02cbf1fb9f
Caner Ay Celep <72454642+caner24@users.noreply.github.com>
2024-04-24 13:56:44 +0300

    Product pages hooks added.

--- src/pages/Products/Product.js
+++ src/pages/Products/Product.js
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { Fragment, useState, useEffect, useCallback } from "react";
 import { Dialog, Disclosure, Menu, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import Page from "../../components/Page/Page";
@@ -11,49 +11,6 @@ import {
 } from "@heroicons/react/20/solid";
 import axios from "axios";
 
-const products = [
-  {
-    id: 1,
-    name: "Earthen Bottle",
-    href: "#",
-    price: "$48",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
-    imageAlt:
-      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
-  },
-  {
-    id: 2,
-    name: "Nomad Tumbler",
-    href: "#",
-    price: "$35",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
-    imageAlt:
-      "Olive drab green insulated bottle with flared screw lid and flat top.",
-  },
-  {
-    id: 3,
-    name: "Focus Paper Refill",
-    href: "#",
-    price: "$89",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-03.jpg",
-    imageAlt:
-      "Person using a pen to cross a task off a productivity paper card.",
-  },
-  {
-    id: 4,
-    name: "Machined Mechanical Pencil",
-    href: "#",
-    price: "$35",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-04.jpg",
-    imageAlt:
-      "Hand holding black machined steel mechanical pencil with brass tip and top.",
-  },
-  // More products...
-];
 const sortOptions = [
   { name: "Most Popular", href: "#", current: true },
   { name: "Best Rating", href: "#", current: false },
@@ -93,6 +50,30 @@ function classNames(...classes) {
 
 export default function Product() {
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
+
+  const [getProductNumber, setProductNumber] = useState(1);
+  const [getProductPerSize, setProductPerSize] = useState(10);
+  const [products, setProducts] = useState([{}]);
+
+  const GetData = async () => {
+    await axios
+      .get(
+        `https://miniecommerceapi.caprover.caneraycelep.social/api/product/getAllProduct?PageNumber=${getProductNumber}&PageSize=${getProductPerSize}`
+      )
+      .then((response) => {
+        console.log(response);
+        setProducts(response.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const fetchData = useCallback(async () => {
+    await GetData();
+  }, [getProductNumber, getProductPerSize]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   return (
     <div className="bg-white">
@@ -356,23 +337,19 @@ export default function Product() {
 
                     <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
                       {products.map((product) => (
-                        <a
-                          key={product.id}
-                          href={product.href}
-                          className="group"
-                        >
+                        <a key={product.Id} href={product.Id} className="group">
                           <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                             <img
-                              src={product.imageSrc}
-                              alt={product.imageAlt}
+                              src={`https://miniecommerceapi.caprover.caneraycelep.social/${product.ProductPhotos?.[0]?.PhotosUrl}`}
+                              alt={product.ProductPhotos?.[0]?.PhotosUrl}
                               className="h-full w-full object-cover object-center group-hover:opacity-75"
                             />
                           </div>
                           <h3 className="mt-4 text-sm text-gray-700">
-                            {product.name}
+                            {product.ProductName}
                           </h3>
                           <p className="mt-1 text-lg font-medium text-gray-900">
-                            {product.price}
+                            {product.ProductPrice}
                           </p>
                         </a>
                       ))}
